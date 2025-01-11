3676ff6e2a19b037c45cebb195b42736b330122e
Ozan uysal <119009473+ozan-uysal@users.noreply.github.com>
2025-01-11 14:50:08 +0300

    1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,23 +1,60 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] InputAction input;
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    Rigidbody rb;
+    [SerializeField] int forcepower = 5;
+    [SerializeField] int rotationPower = 5;
+    private Vector3 RotatorVector3 = new Vector3(0,0,1);
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable() 
     {
-     input.Enable();    
+     thrust.Enable(); 
+     rotation.Enable();    
     }
 
-    private void Update() 
+    private void FixedUpdate()
     {
-       if (input.IsPressed())
-       {
-            Debug.Log(input.bindings);
-      
-       }
-    
+        
+        ProcessThrust();
+        ProcessRotation();
+
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * forcepower * Time.fixedDeltaTime);
+        }
+    }
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log(rotationInput);
+
+        if(rotationInput < 0)
+        {
+           ApplyRotation(rotationPower);
+        }
+         else if(rotationInput > 0)
+        {
+            ApplyRotation(-rotationPower);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(RotatorVector3 * rotationThisFrame * Time.fixedDeltaTime);
     }
 }
